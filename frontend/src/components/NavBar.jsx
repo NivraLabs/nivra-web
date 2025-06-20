c3da781cd37e72118cd5936bcbf360374e8b6dac
emepi <elmeri.kemppainen@gmail.com>
2025-06-20 12:56:19 +0300

    Nav styles

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/nivra_logo.svg';
 import nivra from '../assets/nivra.svg';
+import menu from '../assets/menu.svg';
 import './NavBar.css';
 
 
@@ -10,6 +11,17 @@ export default function NavBar() {
         <img src={logo} alt="logo"/>
         <img src={nivra} alt="nivra"/>
       </header>
+      <div class="nav-cont">
+        <nav class="nav-menu">
+          <a class="nav-link">Docs</a>
+          <a class="nav-link">Roadmap</a>
+          <a class="nav-link">About</a>
+          <a class="nav-link">Join us</a>
+        </nav>
+        <nav class="nav-menu-mobile">
+          <img class="hb-menu-icon" src={menu} alt="menu" />
+        </nav>
+      </div>
     </div>
   );
 }
