4efbffac7e34ed17130d4862e22859ce5a916e31
emepi <elmeri.kemppainen@gmail.com>
2025-06-20 15:17:27 +0300

    Simple collapsing menu

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import logo from '../assets/nivra_logo.svg';
 import nivra from '../assets/nivra.svg';
 import menu from '../assets/menu.svg';
 import './NavBar.css';
+import { Show, createSignal } from 'solid-js';
 
 
 export default function NavBar() {
@@ -18,11 +19,30 @@ export default function NavBar() {
           <a class="nav-link">About</a>
           <a class="nav-link">Join us</a>
         </nav>
-        <nav class="nav-menu-mobile">
-          <img class="hb-menu-icon" src={menu} alt="menu" />
-        </nav>
+        <BMenu />
       </div>
     </div>
   );
 }
 
+function BMenu() {
+  const [collapsed, setCollapsed] = createSignal(false);
+
+  const showMenu = () => {
+    setCollapsed(!collapsed());
+  }
+
+  return (
+    <nav class="nav-menu-mobile">
+      <img class="hb-menu-icon" src={menu} onClick={showMenu} alt="menu" />
+      <Show when={collapsed()}>
+        <div class="collapse-menu">
+          <a class="nav-link">Docs</a>
+          <a class="nav-link">Roadmap</a>
+          <a class="nav-link">About</a>
+          <a class="nav-link">Join us</a>
+        </div>
+      </Show>
+    </nav>
+  );
+}
