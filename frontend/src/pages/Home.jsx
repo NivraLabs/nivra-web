5611cc201fa2cab01e33c77b3e3ceae20124b9fc
emepi <elmeri.kemppainen@gmail.com>
2025-06-22 14:02:15 +0300

    Desc slide 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import nivra_p17 from '../assets/Nivra_p17_v2-min.jpg';
+import nivra_dispute from '../assets/nivra_dispute.jpg';
+import nivra_p19 from '../assets/nivra-p19.jpg';
+import nivra_p11 from '../assets/nivra_p11.jpg';
 import './Home.css';
 
 
@@ -26,6 +29,42 @@ export default function Home() {
           </p>
         </div>
         <img src={nivra_p17} class="nivra-g-1"/>
+      </div>
+
+      <div class="desc-slide-2">
+        <div class="desc-card-2">
+          <img src={nivra_dispute} class="nivra-g-2"/>
+          <div class="card-2-text">
+            <h3 class="heading-3-2">Revolutionizing Conflict Management</h3>
+            <p class="pre-heading-2">
+              When a conflict arises, Nivra Court randomly selects autonomous Nivsters. 
+              Their incentive-aligned voting makes honesty the rational choice. 
+            </p>
+          </div>
+        </div>
+
+        <div class="desc-card-3">
+          <div class="card-2-text">
+            <h3 class="heading-3-2">AI and Human Synergy</h3>
+            <p class="pre-heading-2">
+              Nivra brings AI agents into the decision-making process. 
+              Human arbitrators contribute nuanced, subjective insight to complex disputes, 
+              while the AI agents make it possible to resolve smaller cases with exceptional speed and efficiency.
+            </p>
+          </div>
+          <img src={nivra_p19} class="nivra-g-2"/>
+        </div>
+
+        <div class="desc-card-2">
+          <img src={nivra_p11} class="nivra-g-2"/>
+          <div class="card-2-text">
+            <h3 class="heading-3-2">Streamlined Resolution Process</h3>
+            <p class="pre-heading-2">
+              Once a majority verdict is reached, the smart contract enforces it instantly and delivers swift, 
+              incorruptible finality without any central authority. 
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
