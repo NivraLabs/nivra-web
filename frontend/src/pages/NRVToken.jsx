429de4bef3261ce6dfcebaf260ad4e6492b72308
NivraLabs <team@nivracourt.io>
2025-07-22 20:06:14 +0300

    Adjusting tokenomics parameters

--- frontend/src/pages/NRVToken.jsx
+++ frontend/src/pages/NRVToken.jsx
@@ -52,10 +52,10 @@ export default function Docs() {
         <div class="nvr-card-display">
           <div class="nvr-card">
             <div class="circular-progress">
-            <CircularProgress start={0} percent={35} size={75} strokeWidth={9} animationDuration={2000}/>
+            <CircularProgress start={0} percent={37} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">350,000,000</span> / 1,000,000,000 (35 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">350,000,000</span> (35 %)</h3>
               <p class="text nrv-text">Treasury</p>
               <p class="text nvr-text">• Long-term ecosystem fund (grants, dev support, research, incentives)</p>
               <p class="text nvr-text">• Vesting: linear release over 60 months</p>
@@ -64,10 +64,10 @@ export default function Docs() {
 
           <div class="nvr-card">
             <div class="circular-progress">
-              <CircularProgress start={35} percent={20} size={75} strokeWidth={9} animationDuration={2000}/>
+              <CircularProgress start={37} percent={20} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">200,000,000</span> / 1,000,000,000 (20 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">200,000,000</span> (20 %)</h3>
               <p class="text nrv-text">Team and Advisors</p>
               <p class="text nvr-text">• Team members’ and advisors’ incentives</p>
               <p class="text nvr-text">• Vesting: 3-month cliff, then linear release over 24 months</p> 
@@ -76,10 +76,10 @@ export default function Docs() {
 
           <div class="nvr-card">
             <div class="circular-progress">
-              <CircularProgress start={55} percent={15} size={75} strokeWidth={9} animationDuration={2000}/>
+              <CircularProgress start={57} percent={15} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">150,000,000</span> / 1,000,000,000 (15 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">150,000,000</span> </h3>
               <p class="text nrv-text">Initial Dex Offering (IDO)</p>
               <p class="text nvr-text">• Public sale to bootstrap decentralization</p>
               <p class="text nvr-text">• Release: 100 % at TGE</p>
@@ -88,10 +88,10 @@ export default function Docs() {
 
           <div class="nvr-card">
             <div class="circular-progress">
-              <CircularProgress start={70} percent={15} size={75} strokeWidth={9} animationDuration={2000}/>
+              <CircularProgress start={72} percent={15} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">150,000,000</span> / 1,000,000,000 (15 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">150,000,000</span> (15 %)</h3>
               <p class="text nrv-text">Private Investors</p>
               <p class="text nvr-text">• Seed & strategic backer</p>
               <p class="text nvr-text">• Vesting: 3-month cliff, then linear release over 12 months</p>
@@ -100,37 +100,25 @@ export default function Docs() {
 
           <div class="nvr-card">
             <div class="circular-progress">
-              <CircularProgress start={85} percent={10} size={75} strokeWidth={9} animationDuration={2000}/>
+              <CircularProgress start={87} percent={10} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">100,000,000</span> / 1,000,000,000 (10 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">100,000,000</span> (10 %)</h3>
               <p class="text nrv-text">Initial Liquidity</p>
               <p class="text nvr-text">• Liquidity provision for DEX pools</p>
-              <p class="text nvr-text">• Release: no vesting, time-locked for 12 months</p>
+              <p class="text nvr-text">• Release: no vesting </p>
             </div>
           </div>
 
           <div class="nvr-card">
             <div class="circular-progress">
-              <CircularProgress start={95} percent={3} size={75} strokeWidth={9} animationDuration={2000}/>
+              <CircularProgress start={97} percent={3} size={75} strokeWidth={9} animationDuration={2000}/>
             </div>
             <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">30,000,000</span> / 1,000,000,000 (3 %)</h3>
+              <h3 class="head-4 nrv-head"><span class="nvr-highlight">30,000,000</span> (3 %)</h3>
               <p class="text nrv-text">Marketing</p>
               <p class="text nvr-text">• Campaigns & affiliates</p>
               <p class="text nvr-text">• Vesting: linear release over 12 months</p>
-            </div>
-          </div>
-
-          <div class="nvr-card">
-            <div class="circular-progress">
-              <CircularProgress start={98} percent={2} size={75} strokeWidth={9} animationDuration={2000}/>
-            </div>
-            <div class="nvr-card-text">
-              <h3 class="head-4 nrv-head"><span class="nvr-highlight">20,000,000</span> / 1,000,000,000 (2 %)</h3>
-              <p class="text nrv-text">Launchpool</p>
-              <p class="text nvr-text">• Launchpool rewards</p>
-              <p class="text nvr-text">• Release: no vesting</p>
             </div>
           </div>
 
