18afa49890aa483de4e60d4b53adcfc86e938c4d
NivraLabs <team@nivracourt.io>
2025-07-24 21:15:06 +0300

    Adjusting roadmap

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -51,10 +51,9 @@ export default function Roadmap() {
             <div class="roadmap-fork-right"></div>
             <h3 class="head-3 head-3-right no-wrap">2025 Q4</h3>
             <ul class="roadmap-list roadmap-list-right">
-              <li class="text-4">Conduct smart contract audit</li>
+              <li class="text-4">Publish protocol usage guide for end-users</li>
               <li class="text-4">Private sale</li>
               <li class="text-4">Form strategic partnerships</li>
-              <li class="text-4">Publish protocol usage guide for end-users</li>
               <li class="text-4">Initiate development of smart contracts</li>
               <li class="text-4">Initiate development of protocol integration APIs</li>
             </ul> 
@@ -68,8 +67,9 @@ export default function Roadmap() {
             <ul class="roadmap-list">
               <li class="text-4">Initiate development of CLI and deployment scripts</li>
               <li class="text-4">Initiate development of protocol data indexer</li>
-              <li class="text-4">Initiate development of developer SDKs and integration libraries</li>
+              <li class="text-4">Token smart contract audit</li>
               <li class="text-4">IDO</li>
+              <li class="text-4">Initiate development of developer SDKs and integration libraries</li>
             </ul> 
           </div>
 
